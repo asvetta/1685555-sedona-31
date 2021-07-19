e960ff9aab59962954f65c7e51d15eb0cce1f6f2
asvetta <asvetta@gmail.com>
2021-07-19 17:53:46 +0300

    д2, карта

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-var popup = document.querySelector('.modal');
-var openPopupButton = document.querySelector('.modal-open');
-var form = document.querySelector('.find-hotel-form');
+const popup = document.querySelector('.modal');
+const openPopupButton = document.querySelector('.modal-open');
+const form = document.querySelector('.find-hotel-form');
 
 openPopupButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  form.classList.toggle('visually-hidden');
+  form.classList.toggle('modal-hide');
 });
-
