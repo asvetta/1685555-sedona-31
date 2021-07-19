60199055f9087d0c79e1e758a966525e3aab05a7
asvetta <asvetta@gmail.com>
2021-07-19 23:25:32 +0300

    Третья попытка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const popup = document.querySelector('.modal');
 const openPopupButton = document.querySelector('.modal-open');
 const form = document.querySelector('.find-hotel-form');
+const img = document.querySelector('.map-img');
+
+img.classList.add('map-hide');
+
+form.classList.add('modal-hide');
 
 openPopupButton.addEventListener('click', function (evt) {
   evt.preventDefault();
